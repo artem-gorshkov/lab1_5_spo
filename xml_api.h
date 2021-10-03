daca73e32797be40c1474823ef7ed9a50fc6ec16
a.v.gorshkov <a.v.gorshkov@tinkoff.ru>
2021-10-03 13:04:34 +0300

    edit xml_api.h

--- xml_api.h
+++ xml_api.h
@@ -6,7 +6,8 @@
 //	<action>{0/1/2/3/4/5}</action>
 //	...
 //</request>
-//CREATE TABLE:
+//
+//CREATE TABLE
 //<request>
 //	<action>0</action>
 //	<table>name</table>
@@ -18,11 +19,13 @@
 //		...
 //	</columns>
 //</request>
+//
 //DROP TABLE
 //<request>
 //	<action>1</action>
 //	<table>name</table>
 //</request>
+//
 //INSERT
 //<request>
 //	<action>2</action>
@@ -36,22 +39,76 @@
 //		...
 //	</values>
 //</request>
+//
 //DELETE
 //<request>
 //	<action>3</action>
 //	<table>name</table>
 //	<where>where</where>
 //</request>
+//
 //<response>
 //	<amount>int</amount>
 //</response>
+//
 //SELECT
 //<request>
 //	<action>4</action>
 //	<table>name</table>
+//	<columns>
+//		<column>name</column>
+//	</columns>
+//	<where>where</where>
+//	<offset>int</offset>
+//	<limit>10</limit>
+//	<joins>
+//		<join>
+//			<table>name</table>
+//			<t_column>column</t_column>
+//			<s_column>column</s_column>
+//		</join>
+//	</joins>
+//</request>
+//
+//<response>
+//	<columns>
+//		<column>column</column>
+//		...
+//	</columns>
+//	<values>
+//		<value>value</value>
+//		...
+//	</values>
+//</response>
+//
+//UPDATE
+//<request>
+//	<action>5</action>
+//	<table>name</table>
+//		<columns>
+//		<column>column</column>
+//		...
+//	</columns>
+//	<values>
+//		<value>value</value>
+//		...
+//	</values>
 //	<where>where</where>
 //</request>
 //
 //<response>
-//
+//	<amount>int</amount>
 //</response>
+//
+//WHERE
+//<where>
+//	<op>{0/1/2/3/4/5}</op>
+//	<column>name</column>
+//	<value>value</value>
+//</where>
+//
+//<where>
+//	<op>{6/7}</op>
+//	<left>where</left>
+//	<right>where</right>
+//</where>
